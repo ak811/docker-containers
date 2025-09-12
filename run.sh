9fbb90f57b50e0ed8332c8f93ddeb0f599260ad7
ak811 <akhalegh@charlotte.edu>
2025-09-12 16:20:33 -0400

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/sh
+set -e
+
+# Clean containers/volumes
+docker compose down -v || true
+
+# Reset output folder
+rm -rf out
+mkdir -p out
+
+# Build and run stack
+docker compose up --build
